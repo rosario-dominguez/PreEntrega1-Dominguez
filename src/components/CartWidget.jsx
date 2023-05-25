7950e7fbf393e393c1449bc321bfde52d1bfa23f
rosario dominguez <charo.dma@hotmail.com>
2023-05-24 21:54:19 -0300

    agregue icono carrito

---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,12 @@
+import carticon from "./assets/carticon.svg";
+
+const CartWidget = () => {
+    return (
+        <div>
+            <img src= {carticon} alt="cart-widget" />
+            0
+        </div>
+    )
+}
+
+export default CartWidget
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import CartWidget from './CartWidget'
 
 const Navbar = () => {
   return (
     <nav className="navbar">
         <Link to="/" className="logo"><h1>TecnoShop</h1> </Link>
-        <ul className="menu">
-            <li><Link className="menu-link" to="/">Inicio</Link></li>
-            <li><Link className="menu-link" to="/nosotros">Nosotros</Link></li>
-            <li><Link className="menu-link" to="/productos">Productos</Link></li>
-            <li><Link className="menu-link" to="/productos/Consolas">Consolas</Link></li>
-            <li><Link className="menu-link" to="/productos/Accesorios">Accesorios</Link></li>
-            <li><Link className="menu-link" to="/productos/Juegos">Juegos</Link></li>
-        </ul>
+        <div>
+          <ul className="menu">
+              <li><Link className="menu-link" to="/">Inicio</Link></li>
+              <li><Link className="menu-link" to="/nosotros">Nosotros</Link></li>
+              <li><Link className="menu-link" to="/productos">Productos</Link></li>
+              <li><Link className="menu-link" to="/productos/Consolas">Consolas</Link></li>
+              <li><Link className="menu-link" to="/productos/Accesorios">Accesorios</Link></li>
+              <li><Link className="menu-link" to="/productos/Juegos">Juegos</Link></li>
+          </ul>
+        </div>
+        <CartWidget />
     </nav>
   )
 }
